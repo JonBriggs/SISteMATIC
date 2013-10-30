83d796d225b956fcbc4307eaeb3c1deeb88dada0
crambo70 <cmoredock@Camerons-MacBook-Air.local>
2013-10-30 12:00:00 -0700

    Recommit

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < Individual
   ## added this for the _url redirects to work. Otherwise attempts to redirect to undefined method student_url
+  ## However, this may have implications: see http://goo.gl/ti4tL
+  ## This may be a better way TDI, but was grossly more complex:http://goo.gl/9XJUmJ
   def self.model_name
-    Individual.model_name
+   Individual.model_name
   end
 end
