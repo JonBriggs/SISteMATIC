5b73d28e922c3442665a88ab1aea1137887318b1
crambo70 <cmoredock@Camerons-MacBook-Air.local>
2013-10-30 11:55:49 -0700

    Added model_name ref to Student class for Individual

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
-class Student < Indvidual
+class Student < Individual
+  ## added this for the _url redirects to work. Otherwise attempts to redirect to undefined method student_url
+  def self.model_name
+    Individual.model_name
+  end
 end
